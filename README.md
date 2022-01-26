ccbeeb7879828ee1dfd106ab2195a592d0ec5e9a
furqana11 <furqanalfuady@gmail.com>
2022-01-26 07:25:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# try to see if i could use github web pages
+hope it gonna works.
+# some simple web pages that countdown to my birthday
---
+++ script.js
@@ -0,0 +1,31 @@
+//var Tanggal = prompt('Masukkan Tanggal yang ingin anda hitung (e.g: 11 Apr 2022)')
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+const ultah = '11 Apr 2022'
+
+function countdown(){
+    const ultahDate = new Date(ultah);
+    const currentDate = new Date();
+
+    const totalSeconds =(ultahDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds/3600/24);
+    const hours = Math.floor(totalSeconds/3600) % 24
+    const minutes = (Math.floor(totalSeconds/60) % 60) % 60; 
+    const seconds = Math.floor(totalSeconds) % 60;
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
